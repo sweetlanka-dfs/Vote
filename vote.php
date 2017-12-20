6ebf8af6b21ffcc0450d1cda638a595443255a1e
sweetlanka-dfs <vasilchenko.svitlana@gmail.com>
2017-12-20 16:29:40 +0200

    1.4

--- vote.php
+++ vote.php
@@ -1,8 +1,10 @@
 <?php
-include 'get_Ip.php';
-
+error_reporting(E_ALL | E_STRICT);
+include_once 'get_Ip.php';
 $answer= filter_input(INPUT_POST,'vote', FILTER_SANITIZE_STRING);
-$ipUser = filter_input(INPUT_GET, $ip, FILTER_VALIDATE_IP)
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    echo 'OK';
+}
 
 //---------SET_TIME-----------
 
@@ -11,35 +13,32 @@ $dt = date("Y-m-d H:i:s");
 
 //----------PDO----------------
 $host = 'localhost';
-$db   = 'diamantedesk';
+$db = 'diamantedesk';
 $user = 'root';
 $pass = '';
 $charset = 'utf8';
 
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-
 try {
-    $dsn = new PDO("mysql:host=$host;dbname=$db;", $user, $pass, $opt);
-    // set the PDO error mode to exception
+    $dsn = new PDO("mysql:host=$host;dbname=$db;", $user, $pass);
     $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $dsn->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     $sql = "INSERT INTO `articlevote`(`answer`, `ip`, `date`)
-    VALUES (:answer, :ipUser, :dt)";
+    VALUES (:answer, :ip, :dt)";
     $stmt = $dsn->prepare($sql);
     $stmt->bindParam(':answer', $answer, PDO::PARAM_STR);
-    $stmt->bindParam(':ipUser', $ipUser, PDO::PARAM_STR);
+    $stmt->bindParam(':ip', $ip, PDO::PARAM_STR);
     $stmt->bindParam(':dt', $dt, PDO::PARAM_STR);
+    $stmt->execute();
 }
-catch(PDOException $e)
-{
-    echo $sql . "<br>" . $e->getMessage();
+catch(PDOException $e) {
+    die('Connect ERROR with DB: ' . $e->getMessage());
 }
 
 $dsn = null;
+
 ?>
+
+
 
 
